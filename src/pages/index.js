c8c159426b2400d354c506a503cf73f8df5e1ed3
Murat Akdeniz <makdeniz@msn.com>
2023-05-28 18:13:30 +0200

    update
    Signed-off-by: Murat Akdeniz <makdeniz@msn.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { useEffect, useState } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   const [data, setData] = useState("");
 
   useEffect(() => {
-    fetch('https://backend.argouniverse.com/')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
+    callBackend();
+  }, []); // Empty dependency array to run the effect only once on component mount
+
+  function callBackend() {
+    fetch("https://backend.argouniverse.com/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
         // Handle the retrieved data
         setData(data);
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle any errors
-        console.log('Error:', error);
+        console.log("Error:", error);
       });
-  }, []); // Empty dependency array to run the effect only once on component mount
-
+  }
 
   return (
     <>
@@ -34,30 +36,43 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            {data.quote}
-            <code className={styles.code}>https://github.com/argo-universe/backend-app</code>
-          </p>
-          <div>
-          
+        <center>
+          <a
+            target="_blank"
+            href="https://github.com/argo-universe"
+          >
+            <Image
+              style={{ margin: "100px" }}
+              src="/argo-universe.png"
+              alt="Next.js Logo"
+              width={550}
+              height={255}
+              priority
+            />
+          </a>
+          <div className={styles.description}>
+            <p style={{ padding: "10px", margin: "30px 0px" }}>{data.quote}</p>
+            <p>
+              <a
+                target="_blank"
+                href="https://github.com/argo-universe/backend-app"
+              >
+                https://github.com/argo-universe/backend-app
+              </a>
+            </p>
+            <div></div>
           </div>
-        </div>
+          <button
+            style={{ padding: "10px", margin: "10px" }}
+            onClick={callBackend}
+          >
+            Get Another
+          </button>
+        </center>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/argo-universe.png"
-            alt="Next.js Logo"
-            width={550}
-            height={255}
-            priority
-          />
-        </div>
+        <div className={styles.center}></div>
         <p></p>
-
- 
       </main>
     </>
-  )
+  );
 }
